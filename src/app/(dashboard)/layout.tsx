ac5862afaa5d602837a4c072cc8d9fb559eff071
Austin-12 <austin.ramirez1@outlook.com>
2025-10-20 23:34:04 -0500

    Sidebar file done

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import Navbar from "@/components/Navbar"
-import { Sidebar, SidebarProvider } from '@/components/ui/sidebar'
+"use client";
+
+import React from 'react';
+import Navbar from "@/components/Navbar";
+import { SidebarProvider } from '@/components/ui/sidebar';
+import Sidebar from "@/components/AppSidebar";
+import { NAVBAR_HEIGHT } from '@/lib/constants';
+import { useGetAuthUserQuery } from '@/state/api';
+
 
 
 const DashboardLayout = ({ children } : {children: React.ReactNode}) => {
-  return (
+  const{ data: authUser } = useGetAuthUserQuery();
+  
+  if(!authUser?.userRole) return null;
+
+    return (
     <SidebarProvider> 
     <div className = "min-h-screen w-full bg-primary-100">
       <Navbar />
       <div style = {{ paddingTop: '${NAVBAR_HEIGHT}px'}}>
         <main className = "flex">
-            <Sidebar />
+            <Sidebar userType= {authUser.userRole.toLowerCase()}/>
             <div className = "flex-grow transition-all duration-300">
                 {children}
             </div>
