94b4fec61b337879f27c3a10b5d4b9c117e5a474
Austin-12 <austin.ramirez1@outlook.com>
2025-10-20 23:08:19 -0500

    Some Sidebar stuff done

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from "@/components/Navbar"
+import { Sidebar, SidebarProvider } from '@/components/ui/sidebar'
+
+
+const DashboardLayout = ({ children } : {children: React.ReactNode}) => {
+  return (
+    <SidebarProvider> 
+    <div className = "min-h-screen w-full bg-primary-100">
+      <Navbar />
+      <div style = {{ paddingTop: '${NAVBAR_HEIGHT}px'}}>
+        <main className = "flex">
+            <Sidebar />
+            <div className = "flex-grow transition-all duration-300">
+                {children}
+            </div>
+        </main>
+      </div>
+    </div>
+    </SidebarProvider>
+  )
+}
+
+export default DashboardLayout
