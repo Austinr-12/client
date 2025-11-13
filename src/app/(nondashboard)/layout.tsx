b2291a8be72a3cf656e478b0aa7905c8af4a9751
Austin-12 <austin.ramirez1@outlook.com>
2025-11-12 21:52:28 -0600

    updated files

--- src/app/(nondashboard)/layout.tsx
+++ src/app/(nondashboard)/layout.tsx
@@ -1,47 +1,44 @@
 "use client";
 
-import Navbar from '@/components/Navbar'
-import React, { useEffect, useState } from 'react'
+import Navbar from "@/components/Navbar";
 import { NAVBAR_HEIGHT } from "@/lib/constants";
-import { useGetAuthUserQuery } from '@/state/api';
-import { usePathname, useRouter } from 'next/navigation';
-
+import { useGetAuthUserQuery } from "@/state/api";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const{ data: authUser, isLoading: authLoading } = useGetAuthUserQuery();
+  const { data: authUser, isLoading: authLoading } = useGetAuthUserQuery();
   const router = useRouter();
-    const pathname = usePathname();
-    const [isLoading, setIsLoading] = useState(true);
-  
-    useEffect(() => {
-      if (authUser) {
-        const userRole = authUser.userRole?.toLowerCase();
-        if (
-          (userRole === "manager" && pathname.startsWith("/search")) || 
-          (userRole === "manager" && pathname === "/")
-        ){
-          router.push(
-            "/managers/properties", { scroll: false }
-          );
-        } else {
-          setIsLoading(false)
-        }
+  const pathname = usePathname();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (authUser) {
+      const userRole = authUser.userRole?.toLowerCase();
+      if (
+        (userRole === "manager" && pathname.startsWith("/search")) ||
+        (userRole === "manager" && pathname === "/")
+      ) {
+        router.push("/managers/properties", { scroll: false });
+      } else {
+        setIsLoading(false);
       }
-    }, [authUser, router, pathname]);
-  
-    if(authLoading || isLoading) return <>Loading...</>
-  
+    }
+  }, [authUser, router, pathname]);
+
+  if (authLoading || isLoading) return <>Loading...</>;
+
   return (
     <div className="h-full w-full">
       <Navbar />
-      <main 
-          className={'h-full flex w-full flex-col'}
-          style={{ paddingTop: '${NAVBAR_HEIGHT}px'}}
+      <main
+        className={`h-full flex w-full flex-col`}
+        style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}
       >
         {children}
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
