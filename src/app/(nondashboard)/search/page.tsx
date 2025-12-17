183deda47ec4d6e987f8864f2b6f2287a2490f26
Austin-12 <austin.ramirez1@outlook.com>
2025-12-17 14:06:17 -0600

    added missing lodash and FiltersFull file to work with page.tsx

--- src/app/(nondashboard)/search/page.tsx
+++ src/app/(nondashboard)/search/page.tsx
@@ -5,21 +5,39 @@ import { useAppDispatch, useAppSelector } from "@/state/redux";
 import { useSearchParams } from "next/navigation";
 import React, { useEffect } from "react";
 import FiltersBar from "./FiltersBar";
-import FiltersFull from "./FiltersFull";
 import { cleanParams } from "@/lib/utils";
 import { setFilters } from "@/state";
-import Map from "./Map";
-import Listings from "./Listings";
+import FiltersFull from "./FiltersFull";
 
 
 const SearchPage = () => {
-    const searchParams = useSearchParams();
-    const dispatch = useAppDispatch();
-    const isFiltersFullOpen = useAppSelector(
-        (state) => state.global.isFiltersFullOpen
-    )
+  const searchParams = useSearchParams();
+  const dispatch = useAppDispatch();
+  const isFiltersFullOpen = useAppSelector(
+    (state) => state.global.isFiltersFullOpen
+  );
 
-      return (
+  useEffect(() => {
+    const initialFilters = Array.from(searchParams.entries()).reduce(
+      (acc: any, [key, value]) => {
+        if (key === "priceRange" || key === "squareFeet") {
+          acc[key] = value.split(",").map((v) => (v === "" ? null : Number(v)));
+        } else if (key === "coordinates") {
+          acc[key] = value.split(",").map(Number);
+        } else {
+          acc[key] = value === "any" ? null : value;
+        }
+
+        return acc;
+      },
+      {}
+    );
+
+    const cleanedFilters = cleanParams(initialFilters);
+    dispatch(setFilters(cleanedFilters));
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
     <div
       className="w-full mx-auto px-5 flex flex-col"
       style={{
