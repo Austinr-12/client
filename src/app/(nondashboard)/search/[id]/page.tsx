8ab8d2a7d20d7694cc8e031c53f819d12346bbfb
Austin-12 <austin.ramirez1@outlook.com>
2025-12-30 12:44:28 -0600

    added property Locations to Id folder

--- src/app/(nondashboard)/search/[id]/page.tsx
+++ src/app/(nondashboard)/search/[id]/page.tsx
@@ -4,6 +4,8 @@ import { useParams } from "next/navigation";
 import { useGetAuthUserQuery } from '@/state/api';
 import ImagePreviews from './ImagePreviews';
 import PropertyOverview from './PropertyOverview';
+import PropertyDetails from './PropertyDetails';
+import PropertyLocation from './PropertyLocation';
 
 const page = () => {
     const { id } = useParams();
@@ -17,8 +19,9 @@ const page = () => {
         />
       <div className = "flex flex-col md:flex-row justify-center gap-10 mx-10 md:w-2/3 md:mx-auto mt-16 mb-8">
         <div className = "order-2 md:order-1">
-            <PropertyOverview PropertyId = {propertyId} />
-            <PropertyDetails PropertyId = {propertyId} />
+            <PropertyOverview propertyId = {propertyId} />
+            <PropertyDetails propertyId = {propertyId} />
+            <PropertyLocation propertyId={propertyId} />
         </div>
       </div>
     </div>
