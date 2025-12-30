ed02cb680d3f5400e79a3898272165d403362af9
Austin-12 <austin.ramirez1@outlook.com>
2025-12-30 12:55:14 -0600

    added ContactWidget Section

--- src/app/(nondashboard)/search/[id]/PropertyLocation.tsx
+++ src/app/(nondashboard)/search/[id]/PropertyLocation.tsx
@@ -19,7 +19,7 @@ const PropertyLocation = ({ propertyId }: PropertyDetailsProps) => {
 
     const map = new mapboxgl.Map({
       container: mapContainerRef.current!,
-      style: "mapbox://styles/majesticglue/cm6u301pq008b01sl7yk1cnvb",
+      style: "mapbox://styles/austin-12/cmjbtjo2i000x01qtgae3dx4y",
       center: [
         property.location.coordinates.longitude,
         property.location.coordinates.latitude,
